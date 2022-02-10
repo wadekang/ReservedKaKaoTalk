4e257352fa40a5a2a81232c90d44291a83671873
Wade Kang <49421226+wadekang@users.noreply.github.com>
2022-02-10 20:27:07 +0900

    version update

--- 카카오톡 예약전송.py
+++ 카카오톡 예약전송.py
@@ -1,8 +1,126 @@
-import os, time, sys
+import sys, os, time
 import pyautogui
 import subprocess
 import pyperclip
 import schedule
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5 import uic
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QIcon
+
+num_reserved = None
+chatroom = None
+chatmsg = None
+timelist = None
+shutdown = None
+
+icon_path = None
+mainui = None
+inputui = None
+confirmui = None
+chatimg = None
+searchimg = None
+
+class StartWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        uic.loadUi(mainui, self)
+
+        self.startButton.clicked.connect(self.start_program)
+        
+    def start_program(self):
+        widget.addWidget(InputWindow(self.numReserve.value()))
+
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        widget.setFixedHeight(630)
+        widget.setFixedWidth(470)
+        
+class InputWindow(QWidget):
+    def __init__(self, numReserve):
+        super().__init__()
+        uic.loadUi(inputui, self)
+        self.backButton.clicked.connect(self.back_button)
+        self.nextButton.clicked.connect(self.next_button)
+
+        self.numReserve = numReserve
+        self.chatRoomList = ['']*(self.numReserve+1)
+        self.chatMsgList = ['']*(self.numReserve+1)
+        self.timeList = ['']*(self.numReserve+1)
+        self.curIdx = 1
+        for i in range(self.numReserve):
+            self.messageSelect.addItem('예약 ' + str(i+1))
+
+        self.messageSelect.activated[str].connect(self.message_select_activated)
+
+    def back_button(self):
+        widget.setCurrentIndex(widget.currentIndex() - 1)
+        widget.removeWidget(self)
+        widget.setFixedHeight(470)
+        widget.setFixedWidth(470)
+
+    def next_button(self):
+        self.chatRoomList[self.curIdx] = self.chatroomName.toPlainText()
+        self.chatMsgList[self.curIdx] = self.chatMessage.toPlainText()
+        self.timeList[self.curIdx] = self.reservedTime.toPlainText()
+
+        widget.addWidget(ConfirmWindow(self.numReserve, self.chatRoomList, self.chatMsgList, self.timeList, self.shutdownCheck.isChecked()))
+
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        widget.setFixedHeight(300)
+        widget.setFixedWidth(400)
+
+    def message_select_activated(self, text):
+        self.chatRoomList[self.curIdx] = self.chatroomName.toPlainText()
+        self.chatMsgList[self.curIdx] = self.chatMessage.toPlainText()
+        self.timeList[self.curIdx] = self.reservedTime.toPlainText()
+        
+        nextIdx = int(text.split()[1])
+        self.chatroomName.setPlainText(self.chatRoomList[nextIdx])
+        self.chatMessage.setPlainText(self.chatMsgList[nextIdx])
+        self.reservedTime.setPlainText(self.timeList[nextIdx])
+
+        self.curIdx = nextIdx
+
+class ConfirmWindow(QWidget):
+    def __init__(self, numReserve, chatRoomList, chatMsgList, timeList, shutdown):
+        super().__init__()
+        uic.loadUi(confirmui, self)
+        self.backButton.clicked.connect(self.back_button)
+        self.confirmButton.clicked.connect(self.confirm_button)
+
+        self.alert_msg = '===========예약내역===========\n\n'
+        for i in range(numReserve):
+            temp = '* 채팅방: ' + chatRoomList[i+1] + ', 시간: ' + timeList[i+1] + '\n'
+            self.alert_msg += temp
+
+        self.alertMessage.setPlainText(self.alert_msg)
+
+        self.numReserve = numReserve
+        self.chatRoomList = chatRoomList
+        self.chatMsgList = chatMsgList
+        self.timeList = timeList
+        self.shutdown = shutdown
+
+    def back_button(self):
+        widget.setCurrentIndex(widget.currentIndex() - 1)
+        widget.removeWidget(self)
+        widget.setFixedHeight(630)
+        widget.setFixedWidth(470)
+
+    def confirm_button(self):
+        QMessageBox.information(self, '카카오톡 예약전송', '예약 되었습니다.')
+
+        global num_reserved, chatroom, chatmsg, timelist, shutdown
+        num_reserved = self.numReserve
+        chatroom = self.chatRoomList
+        chatmsg = self.chatMsgList
+        timelist = self.timeList
+        shutdown = self.shutdown
+
+        app.exit()
 
 def init():
     pyautogui.FAILSAFE = False
@@ -17,8 +135,7 @@ def kakao_run():
         print('[ERROR] Execute KaKaoTalk')
         raise
     
-def img_click(img_file):
-    img_path = os.path.join('img', img_file)
+def img_click(img_path):
     location = pyautogui.locateCenterOnScreen(img_path, confidence=0.65)
     x, y = location
     
@@ -40,56 +157,86 @@ def send_message_and_exit(msg):
     pyautogui.press('esc')
 
 def reserved_transmission(info):
-    chag_img = 'chat.png'
-    search_img = 'search.png'
-    
     kakao_run()
     
-    img_click(chag_img)
-    img_click(search_img)
+    img_click(chatimg)
+    img_click(searchimg)
     chatroom_search_and_enter(info['chatroom_name'])
     send_message_and_exit(info['msg'])
    
     return schedule.CancelJob
 
-def main():
-    init()
-    num = pyautogui.prompt('예약할 메세지 개수를 입력하세요.', '카카오톡 예약전송')
+def data_load():
+    global icon_path, mainui, inputui, confirmui, chatimg, searchimg
+
+    try:
+        icon_path = os.path.join(getattr(sys, '_MEIPASS'), 'icon\paper-plane.png')
+        mainui = os.path.join(getattr(sys, '_MEIPASS'), 'ui\mainui.ui')
+        inputui = os.path.join(getattr(sys, '_MEIPASS'), 'ui\inputui.ui')
+        confirmui = os.path.join(getattr(sys, '_MEIPASS'), 'ui\confirmui.ui')
+        chatimg = os.path.join(getattr(sys, '_MEIPASS'), 'img\chat.png')
+        searchimg = os.path.join(getattr(sys, '_MEIPASS'), 'img\search.png')
+
+    except:
+        icon_path = 'icon\paper-plane.png'
+        mainui = 'ui\mainui.ui'
+        inputui = 'ui\inputui.ui'
+        confirmui = 'ui\confirmui.ui'
+        chatimg = 'img\chat.png'
+        searchimg = 'img\search.png'
+
+def program_exited():
+    global num_reserved, chatroom, chatmsg, timelist, shutdown
+    if num_reserved is None or chatroom is None or chatmsg is None or timelist is None or shutdown is None:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__" :
+    data_load()
+
+    app = QApplication(sys.argv)
     
-    all_data = []
-    for i in range(int(num)):
-        chatroom_name = pyautogui.prompt('채팅방 이름을 입력하세요.', '카카오톡 예약전송')
-        msg = pyautogui.prompt('내용을 입력하세요.', '카카오톡 예약전송')
-        reserved_time = pyautogui.prompt('예약할 시간을 입력하세요. (ex: 16:30)', '카카오톡 예약전송')
+    widget = QtWidgets.QStackedWidget()
+    widget.setWindowTitle('카카오톡 예약전송')
+    widget.setWindowIcon(QIcon(icon_path))
+
+    startWindow = StartWindow()
+
+    widget.addWidget(startWindow)
+
+    widget.setFixedHeight(470)
+    widget.setFixedWidth(470)
+    widget.show()
+    
+    app.exec_()
+
+    widget.hide()
+
+    if program_exited():
+        sys.exit()
+
+    init()
+    for i in range(num_reserved):
+        chatroom_name = chatroom[i+1]
+        msg = chatmsg[i+1]
+        reserved_time = timelist[i+1]
         
         info = {}
         info['chatroom_name'] = chatroom_name
         info['msg'] = msg
         info['reserved_time'] = reserved_time
         
-        all_data.append(info)
-        
         schedule.every().day.at(reserved_time).do(reserved_transmission, info)
-    
-    shutdown = pyautogui.confirm('전송 후 컴퓨터를 종료할까요?', '카카오톡 예약전송', ['Yes', 'No'])
-    alert_msg = '예약이 완료되었습니다.\n\n=====예약내역=====\n\n'
-    
-    for data in all_data:
-        temp = '* 채팅방: ' + data['chatroom_name'] + ', 시간: ' + data['reserved_time'] + '\n'
-        alert_msg += temp
-        
-    pyautogui.alert(alert_msg, '카카오톡 예약전송')
-    
+
     while True:
         schedule.run_pending()
-        time.sleep(10)
+        time.sleep(5)
         
         all_jobs = schedule.get_jobs()
         if not all_jobs:
-            if shutdown == 'Yes':
+            if shutdown:
                 os.system('shutdown -s -f')
             else:
                 sys.exit()
-    
-if __name__ == '__main__':
-    main()
+
